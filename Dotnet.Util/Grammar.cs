bb29d85d1940c1fe6efcc5fd1623d82b4b1180d7
Dwakel <avokeselorm@live.com>
2020-01-24 13:45:13 +0000

    Add project files.

---
+++ Dotnet.Util/Grammar.cs
@@ -0,0 +1,82 @@
+﻿namespace Dotnet.Util
+{
+    public static class Grammar
+    {
+        #region Pluralize
+        public static string Pluralize(int count, string singular, string plural = "")
+        {
+            if (count != 1)
+                return plural == "" ? singular + "s" : plural;
+            return singular;
+            
+        }
+        public static string Pluralize(double count, string singular, string plural = "")
+        {
+            if (count != 1)
+                return plural == "" ? singular + "s" : plural;
+            return singular;
+
+        }
+        public static string Pluralize(string count, string singular, string plural = "")
+        {
+            if (!count.ToLower().Equals("one"))
+                return plural == "" ? singular + "s" : plural;
+            return singular;
+
+        }
+        public static string Pluralize(this string singular, int count, string plural = "")
+        {
+            if (count != 1)
+                return plural == "" ? singular + "s" : plural;
+            return singular;
+
+        }
+        public static string Pluralize(this string singular, double count, string plural = "")
+        {
+            if (count != 1)
+                return plural == "" ? singular + "s" : plural;
+            return singular;
+
+        }
+        #endregion Pluralize
+
+        #region Singularize
+        public static string Singularize(int count, string plural, string singular = "")
+        {
+            if (count == 1)
+                return singular == "" ? plural.TrimEnd('s') : singular;
+            return singular;
+
+        }
+        public static string Singularize(double count, string plural, string singular = "")
+        {
+            if (count == 1)
+                return singular == "" ? plural.TrimEnd('s') : singular;
+            return singular;
+
+        }
+        public static string Singularize(string count, string plural, string singular = "")
+        {
+            if (count.ToLower().Equals("one"))
+                return singular == "" ? plural.TrimEnd('s') : singular;
+            return singular;
+
+        }
+        public static string Singularize(this string plural, int count, string singular = "")
+        {
+            if (count == 1)
+                return singular == "" ? plural.TrimEnd('s') : singular;
+            return singular;
+
+        }
+        public static string Singularize(this string plural, double count, string singular = "")
+        {
+            if (count == 1)
+                return singular == "" ? plural.TrimEnd('s') : singular;
+            return singular;
+
+        }
+        #endregion Singularize
+
+    }
+}
