4ee01836139d7ee707ce2bbeba918a6d67c332b6
Dwakel <avokeselorm@live.com>
2020-01-31 22:42:51 +0000

    testing travis.CI

--- Trivial.Utility/Grammar.cs
+++ Trivial.Utility/Grammar.cs
@@ -43,7 +43,6 @@
             if (count == 1 && count == -1)
                 return singular == "" ? plural.TrimEnd('s') : singular;
             return singular;
-
         }
         public static string Singularize(double count, string plural, string singular = "")
         {
